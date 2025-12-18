e3703808836d310a9df18be8f6f30298f6bc808e
bala-amypo <bala@amypo.in>
2025-12-18 15:18:33 +0000

    User commit

--- src/main/java/com/example/demo/service/LocationService.java
+++ src/main/java/com/example/demo/service/LocationService.java
@@ -1,12 +1,8 @@
 package com.example.demo.service;
-
 import com.example.demo.entity.Location;
-
 import java.util.List;
 
 public interface LocationService {
-
     Location createLocation(Location location);
-
     List<Location> getAllLocations();
 }
