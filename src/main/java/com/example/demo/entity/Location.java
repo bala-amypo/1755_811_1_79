24aee222705cfb2d57161bb9385c9f5cfa4df357
bala-amypo <bala@amypo.in>
2025-12-18 15:34:16 +0000

    User commit

--- src/main/java/com/example/demo/entity/Location.java
+++ src/main/java/com/example/demo/entity/Location.java
@@ -1,10 +1,8 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
-@Data
 public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,4 +10,13 @@ public class Location {
     private String name;
     private Double latitude;
     private Double longitude;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Double getLatitude() { return latitude; }
+    public void setLatitude(Double latitude) { this.latitude = latitude; }
+    public Double getLongitude() { return longitude; }
+    public void setLongitude(Double longitude) { this.longitude = longitude; }
 }
