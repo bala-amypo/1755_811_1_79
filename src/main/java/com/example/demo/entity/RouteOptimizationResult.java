dff5cedd53dcf8e601cf4f35cf3f6636179a8498
bala-amypo <bala@amypo.in>
2025-12-21 16:20:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/RouteOptimizationResult.java
+++ src/main/java/com/example/demo/entity/RouteOptimizationResult.java
@@ -1,7 +1,10 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "route_optimization_results")
@@ -12,7 +15,8 @@ public class RouteOptimizationResult {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "shipment_id", nullable = false)
+    @JoinColumn(name = "shipment_id")
+    @JsonIgnore
     private Shipment shipment;
 
     @Column(nullable = false)
@@ -24,8 +28,7 @@ public class RouteOptimizationResult {
     @Column(nullable = false)
     private LocalDateTime generatedAt;
 
-    public RouteOptimizationResult() {
-    }
+    public RouteOptimizationResult() {}
 
     public RouteOptimizationResult(Shipment shipment,
                                    Double optimizedDistanceKm,
@@ -42,13 +45,9 @@ public class RouteOptimizationResult {
         this.generatedAt = LocalDateTime.now();
     }
 
-    // Getters and Setters
+    // Getters
     public Long getId() {
         return id;
-    }
-
-    public Shipment getShipment() {
-        return shipment;
     }
 
     public Double getOptimizedDistanceKm() {
@@ -61,17 +60,5 @@ public class RouteOptimizationResult {
 
     public LocalDateTime getGeneratedAt() {
         return generatedAt;
-    }
-
-    public void setShipment(Shipment shipment) {
-        this.shipment = shipment;
-    }
-
-    public void setOptimizedDistanceKm(Double optimizedDistanceKm) {
-        this.optimizedDistanceKm = optimizedDistanceKm;
-    }
-
-    public void setEstimatedFuelUsageL(Double estimatedFuelUsageL) {
-        this.estimatedFuelUsageL = estimatedFuelUsageL;
     }
 }
