e13d5dde4bce29f7a3187707bfc1db9e356895db
bala-amypo <bala@amypo.in>
2025-12-25 15:55:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
@@ -1,13 +1,11 @@
 package com.example.demo.service.impl;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.example.demo.entity.Location;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.LocationRepository;
 import com.example.demo.service.LocationService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LocationServiceImpl implements LocationService {
@@ -20,27 +18,17 @@ public class LocationServiceImpl implements LocationService {
 
     @Override
     public Location createLocation(Location location) {
-
         if (location.getLatitude() < -90 || location.getLatitude() > 90) {
-            throw new IllegalArgumentException("Invalid latitude value");
+            throw new IllegalArgumentException("Invalid latitude");
         }
-
         if (location.getLongitude() < -180 || location.getLongitude() > 180) {
-            throw new IllegalArgumentException("Invalid longitude value");
+            throw new IllegalArgumentException("Invalid longitude");
         }
-
         return locationRepository.save(location);
     }
 
     @Override
     public List<Location> getAllLocations() {
         return locationRepository.findAll();
-    }
-
-    @Override
-    public Location findById(Long locationId) {
-        return locationRepository.findById(locationId)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Location not found"));
     }
 }
